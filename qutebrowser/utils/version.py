6072f65174372d65677f70136c365d92c4141955
Freya Bruhin <me@the-compiler.org>
2026-09-07 13:26:48 +0200

    tests: Update Chromium version for Qt 6.12 Beta 4

--- qutebrowser/utils/version.py
+++ qutebrowser/utils/version.py
@@ -669,8 +669,8 @@ class WebEngineVersions:
         utils.VersionNumber(6, 11, 1): (_BASES[140], '148.0.7778.96'),  # 2026-04-28
         utils.VersionNumber(6, 11, 2): (_BASES[140], '151.0.7922.71'),  # 2026-07-28
 
-        ## Qt 6.12 (Beta 3)
-        utils.VersionNumber(6, 12): (_BASES[140], '151.0.7922.71'),  # 2026-07-28
+        ## Starting with Qt 6.12 Beta 4, QtWebEngine is versioned independently from Qt
+        utils.VersionNumber(6, 140): (_BASES[140], '151.0.7922.173'),  # 2026-08-19
     }
 
     def __post_init__(self) -> None:
